75c71ee1069fdecc3bae02a66b46f8609ee95a58
Tatiana <tatigerzens@gmail.com>
2024-09-04 14:15:32 -0300

    Added new functionalities: delete, add, and update for the project

--- js/app.js
+++ js/app.js
@@ -13,13 +13,81 @@ const message = document.querySelector('.list-group');
 let tasks = [];
 
 
+eventListeners();
+function eventListeners() {
+  document.addEventListener('DOMContentLoaded', ()=> {
+    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    newHTML();
+  });
+
+  listTasks.addEventListener('click', removeTasks);
+}
+
+
+function removeTasks(e){
+
+  if(e.target.tagName == 'SPAN') {
+     const removeId = parseInt(e.target.getAttribute('task-id'));
+     tasks = tasks.filter(task => task.id !== removeId);
+   
+     newHTML();
+    };
+}
+
+
+
+function removeAll(){
+  
+  tasks = [];
+  newHTML();
+}
 
 function addTasks(){
     const task = input.value;
     if (task === '') {
         showError('Please, complete this field.')
+        return;
     };
+    
+    const taskObject = {
+      task: task,
+      id: Date.now()
+    }
+
+    tasks = [...tasks, taskObject]
+
+    newHTML();
+    input.value = '';
 }
+
+
+function newHTML(){
+      clearHTML();
+
+  if (tasks.length > 0){
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = `${task.task}          
+         <span task-id="${task.id}"> X </span>    
+     `;
+
+      listTasks.appendChild(li);
+    });
+  }
+
+  saveToLocalStorage();
+}
+
+
+function saveToLocalStorage() {
+     localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+
+function clearHTML() {
+    listTasks.innerHTML = '';
+}
+
 
 
 function showError(error){
